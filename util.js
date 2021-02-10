4d7793b4ac63392e9d5a7f44b748627b040f6f7b
anthnyprschka <anthonyproschka@gmail.com>
2021-02-10 10:59:39 +0100

    add function to calc pot odds

---
+++ util.js
@@ -0,0 +1,11 @@
+const calcPotOdds = (gameState) => {
+  const { pot, in_action, players, current_buy_in } = gameState;
+
+  const ourPlayer = players.find((player) => player.id == in_action);
+
+  const amountToCall = current_buy_in - ourPlayer.bet;
+
+  const potOdds = amountToCall / (pot + amountToCall);
+
+  return potOdds;
+};
