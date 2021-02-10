c319db4e4f1d16636f09564d2f9b017139e68386
Pritoj Singh <pritojs@gmail.com>
2021-02-10 11:28:38 +0100

    test setup

---
+++ tests/hand-strength-calc.test.js
@@ -0,0 +1,14 @@
+const { CardGroup } = require("poker-odds-calculator");
+const { getCardGroup, getOurPlayer} = require("../hand -strength-calc");
+const { mockPlayer, mockGameState } = require("./mocks");
+
+describe('Card group', () => {
+  it('should return the right card group', () => {
+    const g = getCardGroup(mockPlayer.hole_cards);
+    expect(g.toString()).toBe('6h Ks')
+  })
+
+  it("should return our player", () => {
+    expect(getOurPlayer(mockGameState).id).toBe(1)
+  })
+});
