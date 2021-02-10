9884fb3fd21e498562abed49d2292c9bab1b4bac
anthnyprschka <anthonyproschka@gmail.com>
2021-02-10 10:49:18 +0100

    call

--- Player.js
+++ Player.js
@@ -1,14 +1,16 @@
 class Player {
   static get VERSION() {
-    return '0.1';
+    return "0.1";
   }
 
   static betRequest(gameState, bet) {
-    bet(0);
+    const highestBet =
+      gameState.players.reduce((f, v) => (f > v.bet ? f : v.bet), 0) + 10;
+
+    bet(highestBet);
   }
 
-  static showdown(gameState) {
-  }
+  static showdown(gameState) {}
 }
 
 module.exports = Player;
