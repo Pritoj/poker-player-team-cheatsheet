1453153582e8de8f7119a540a053526dfb8408b5
anthnyprschka <anthonyproschka@gmail.com>
2021-02-10 11:10:00 +0100

    bet if pot odds are less than 0.5

--- Player.js
+++ Player.js
@@ -1,13 +1,19 @@
+import { calcPotOdds } from "util";
+
 class Player {
   static get VERSION() {
     return "0.1";
   }
 
   static betRequest(gameState, bet) {
-    const highestBet =
-      gameState.players.reduce((f, v) => (f > v.bet ? f : v.bet), 0) + 10;
+    const podOdds,
+      amountToCall = calcPotOdds(gameState);
 
-    bet(highestBet);
+    if (podOdds < 0.5) {
+      bet(amountToCall);
+    }
+
+    bet(0);
   }
 
   static showdown(gameState) {}
